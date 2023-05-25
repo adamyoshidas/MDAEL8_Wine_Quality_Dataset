b84615c6057d8a5100033cd436d12d2eb9754487
adamyoshidas <adam.yoshida@aluno.ifsp.edu.br>
2023-05-25 19:06:29 -0300

    Update DecisionTree.py

--- 3-Classification/DecisionTree.py
+++ 3-Classification/DecisionTree.py
@@ -3,21 +3,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+import pandas as pd 
 
 def main():
-    iris = load_iris()
-    print(iris.data)
-    print(iris.target)
-    X = iris.data
-    y = iris.target
+
+    input_file = '0-Datasets/WineQTClear.data'
+    names = ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']
     
+    df = pd.read_csv(input_file, names = names)
+
+    #iris = load_iris()
+    #print(iris.data)
+    #print(iris.target)
+    #X = iris.data
+    #y = iris.target
+    
+    X = df
+    y = df['quality']
+
     # Split the data - 75% train, 25% test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)    
-    iris = load_iris()
-    print(iris.data)
-    print(iris.target)
-    X = iris.data
-    y = iris.target
+    #iris = load_iris()
+    #print(iris.data)
+    #print(iris.target)
+    #X = iris.data
+    #y = iris.target
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
